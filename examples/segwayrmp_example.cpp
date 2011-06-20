f359605d37b5d7e99028cc38639c19471991d5c8
William Woodall <wjwwood@gmail.com>
2011-06-19 19:19:02 -0500

    More sofisticated example.

--- examples/segwayrmp_example.cpp
+++ examples/segwayrmp_example.cpp
@@ -1,16 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+
 #include "segwayrmp.h"
 
 void handleSegwayStatus(segwayrmp::SegwayStatus &ss) {
     std::cout << ss.str() << std::endl << std::endl;
 }
 
-int main(void) {
-    segwayrmp::SegwayRMP rmp(segwayrmp::serial);
-    rmp.configureSerial("/dev/ttyUSB0");
+int run_segway(segwayrmp::InterfaceType interface_type, std::string port = "/dev/ttyUSB0") {
+    segwayrmp::SegwayRMP rmp(interface_type);
+    if (interface_type == segwayrmp::serial) {
+        rmp.configureSerial(port);
+    }
     rmp.setStatusCallback(handleSegwayStatus);
     rmp.connect();
     while(true) {
         rmp.move(1.0, 0);
         usleep(100000);
+    }
+}
+
+void print_usage() {
+    std::cout << "Usage: segwayrmp_example usb" << std::endl;
+    std::cout << "       or" << std::endl;
+    std::cout << "       segwayrmp_example serial <serial port>" << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        print_usage();
+        return 0;
+    }
+    if (argv[1] == "serial") {
+        if (argc < 3) {
+            print_usage();
+            return 0;
+        }
+        run_segway(segwayrmp::serial, std::string(argv[2]));
+    } else {
+        run_segway(segwayrmp::usb);
     }
 }
