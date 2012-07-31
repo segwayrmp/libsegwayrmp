bb6e1f6cc0da59d329fc7af4bd885c9e9a34786a
William Woodall <wjwwood@gmail.com>
2012-07-31 01:59:01 -0500

    Changed the way the Serial dependency is searched for.  If it is not found with `find_package` and the `ROS_ROOT` environment variable is set then it tries to find the serial library using rosbuild.

--- cmake/segwayrmp_serial.cmake
+++ cmake/segwayrmp_serial.cmake
@@ -1,15 +1,40 @@
 # Find Serial if support requested
 if(SEGWAYRMP_USE_SERIAL)
-  find_package(serial)
+  find_package(serial QUIET)
+
+  set(SEGWAYRMP_USE_SERIAL FALSE)
   if(serial_FOUND)
-    message("-- Building with serial support")
+    set(SEGWAYRMP_USE_SERIAL TRUE)
     include_directories(serial_INCLUDE_DIRS)
-    list(APPEND SEGWAYRMP_SRCS src/impl/rmp_serial.cc)
     list(APPEND SEGWAYRMP_LINK_LIBS ${serial_LIBRARIES})
-    add_definitions(-DSEGWAYRMP_USE_SERIAL)
   else(serial_FOUND)
-    message("--")
-    message("-- Serial support disabled: Serial library not found.")
-    message("--")
+    # Try to use ROS to find it
+    set(ROS_ROOT $ENV{ROS_ROOT})
+    if(DEFINED ROS_ROOT)
+      message("-- Using ROS to resolve serial dependency")
+      # Setup ROS build
+      include($ENV{ROS_ROOT}/core/rosbuild/rosbuild.cmake)
+      rosbuild_init()
+      # Add include and link directories
+      include_directories(${${PROJECT_NAME}_INCLUDE_DIRS})
+      link_directories(${${PROJECT_NAME}_LIBRARY_DIRS})
+      # Add ROS libraries for this "project"
+      list(APPEND SEGWAYRMP_LINK_LIBS ${${PROJECT_NAME}_LIBRARIES})
+      # Set the default path for built executables to the "bin" directory
+      set(EXECUTABLE_OUTPUT_PATH ${PROJECT_SOURCE_DIR}/bin)
+      # set the default path for built libraries to the "lib" directory
+      set(LIBRARY_OUTPUT_PATH ${PROJECT_SOURCE_DIR}/lib)
+    else(DEFINED ROS_ROOT)
+      # Could not find serial either through find_package or ROS
+      message("--")
+      message("-- Serial support disabled: Serial library not found.")
+      message("--")
+    endif(DEFINED ROS_ROOT)
   endif(serial_FOUND)
+
+  if(SEGWAYRMP_USE_SERIAL)
+    message("-- Building with serial support")
+    list(APPEND SEGWAYRMP_SRCS src/impl/rmp_serial.cc)
+    add_definitions(-DSEGWAYRMP_USE_SERIAL)
+  endif(SEGWAYRMP_USE_SERIAL)
 endif(SEGWAYRMP_USE_SERIAL)
