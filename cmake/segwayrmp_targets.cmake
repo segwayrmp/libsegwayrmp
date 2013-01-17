d7cb0f95f66e2831c4660d8f6538d4e5746de1b7
William Woodall <wjwwood@gmail.com>
2013-01-17 13:35:31 -0800

    Preparation for release into groovy as 3rdparty

--- cmake/segwayrmp_targets.cmake
+++ cmake/segwayrmp_targets.cmake
@@ -1,10 +1,5 @@
 ## This file setups the targets like install and uninstall
 
-# Configure make install
-if (NOT CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX /usr/local)
-endif(NOT CMAKE_INSTALL_PREFIX)
-
 install(
   TARGETS segwayrmp
   RUNTIME DESTINATION bin
@@ -17,13 +12,14 @@ install(
   DESTINATION include/segwayrmp
 )
 
-if (NOT CMAKE_FIND_INSTALL_PATH)
-  set(CMAKE_FIND_INSTALL_PATH ${CMAKE_ROOT})
-endif(NOT CMAKE_FIND_INSTALL_PATH)
-
 install(
   FILES       cmake/Findsegwayrmp.cmake
-  DESTINATION ${CMAKE_FIND_INSTALL_PATH}/Modules/
+  DESTINATION share/libsegwayrmp/
+)
+
+install(
+  FILES package.xml
+  DESTINATION share/libsegwayrmp/
 )
 
 # Configure make uninstall
