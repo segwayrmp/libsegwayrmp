974ea8a80262a79d9bb925fc87dde5d5a299167b
William Woodall <wjwwood@gmail.com>
2013-01-17 13:25:58 -0800

    Removed ROS specific code

--- cmake/segwayrmp_serial.cmake
+++ cmake/segwayrmp_serial.cmake
@@ -7,36 +7,16 @@ if(SEGWAYRMP_USE_SERIAL)
     set(SEGWAYRMP_USE_SERIAL TRUE)
     include_directories(serial_INCLUDE_DIRS)
     list(APPEND SEGWAYRMP_LINK_LIBS ${serial_LIBRARIES})
-  else(serial_FOUND)
-    # Try to use ROS to find it
-    set(ROS_ROOT $ENV{ROS_ROOT})
-    if(DEFINED ROS_ROOT)
-      message("-- Using ROS to resolve serial dependency")
-      # Setup ROS build
-      include($ENV{ROS_ROOT}/core/rosbuild/rosbuild.cmake)
-      rosbuild_init()
-      # Add include and link directories
-      include_directories(${${PROJECT_NAME}_INCLUDE_DIRS})
-      link_directories(${${PROJECT_NAME}_LIBRARY_DIRS})
-      # Add ROS libraries for this "project"
-      list(APPEND SEGWAYRMP_LINK_LIBS ${${PROJECT_NAME}_LIBRARIES})
-      # Set the default path for built executables to the "bin" directory
-      set(EXECUTABLE_OUTPUT_PATH ${PROJECT_SOURCE_DIR}/bin)
-      # set the default path for built libraries to the "lib" directory
-      set(LIBRARY_OUTPUT_PATH ${PROJECT_SOURCE_DIR}/lib)
-      # We found it, set true
-      set(SEGWAYRMP_USE_SERIAL TRUE)
-    else(DEFINED ROS_ROOT)
-      # Could not find serial either through find_package or ROS
-      message("--")
-      message("-- Serial support disabled: Serial library not found.")
-      message("--")
-    endif(DEFINED ROS_ROOT)
-  endif(serial_FOUND)
+  else()
+    # Could not find serial either through find_package or ROS
+    message("--")
+    message("-- Serial support disabled: Serial library not found.")
+    message("--")
+  endif()
 
   if(SEGWAYRMP_USE_SERIAL)
     message("-- Building SegwayRMP with serial support")
     list(APPEND SEGWAYRMP_SRCS src/impl/rmp_serial.cc)
     add_definitions(-DSEGWAYRMP_USE_SERIAL)
-  endif(SEGWAYRMP_USE_SERIAL)
-endif(SEGWAYRMP_USE_SERIAL)
+  endif()
+endif()
